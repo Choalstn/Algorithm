8d53194c5c4f65d90de8a63f9e2afb6df9d5d0f2
Choalstn <105564451+Choalstn@users.noreply.github.com>
2023-01-11 20:07:01 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -0,0 +1,12 @@
+function solution(array, height) {
+    let count = 0;
+    
+    for(let i=0; i<array.length; i++) {
+        if(array[i] > height) {
+            count++;
+        } else {
+            continue;
+        }
+    }
+    return count
+}
