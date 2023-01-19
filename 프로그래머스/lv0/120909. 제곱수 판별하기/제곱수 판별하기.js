f88abb879a9dbf8231ad03fa17069a135d634821
조민수 <105564451+Choalstn@users.noreply.github.com>
2023-01-19 22:40:37 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.js
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.js
@@ -1,15 +1,3 @@
 function solution(n) {
-      let arr = [];
-
-  for (let i = 1; i < n; i++) {
-    if (n % i === 0 && i * i === n) {
-      arr.push(i);
-    }
-  }
-
-  if (arr.length > 0) {
-    return 1;
-  } else {
-    return 2;
-  }
+    return Number.isInteger(Math.sqrt(n)) ? 1 : 2;
 }
