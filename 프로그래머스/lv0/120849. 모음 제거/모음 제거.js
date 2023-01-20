b3ecd13d7ed768481c852ec0ec529a93d52ba3b1
조민수 <105564451+Choalstn@users.noreply.github.com>
2023-01-20 17:52:03 +0900

    [level 0] Title: 모음 제거, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
@@ -1,17 +1,5 @@
 function solution(my_string) {
-      let vow = ['a', 'e', 'i', 'o', 'u'];
-  let arr = my_string.split(''); // b u s
-  let result = [];
-
-  for (let i = 0; i < arr.length; i++) {
-    console.log('i : ', arr[i]);
-    for (let j = 0; j < vow.length; j++) {
-      console.log('j : ', vow[j]);
-      if (arr[i] === vow[j]) {
-        result.push(arr[i]);
-      }
-    }
-  }
-
-  return arr.filter((el) => !result.includes(el)).join('');
+    return Array.from(my_string)
+    .filter((t) => !['a', 'e', 'i', 'o', 'u'].includes(t))
+    .join('');
 }
