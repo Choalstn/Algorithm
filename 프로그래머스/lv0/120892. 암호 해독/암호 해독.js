9e5d314f835ec3d712e641a8315d1a362c29bf24
조민수 <105564451+Choalstn@users.noreply.github.com>
2023-01-24 02:49:25 +0900

    [level 0] Title: 암호 해독, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
@@ -1,10 +1,8 @@
 function solution(cipher, code) {
-  let arr = [];
-  for (let i = 1; i <= cipher.length; i++) {
-    if (i % code === 0) {
-      arr.push(cipher[i - 1]);
-    }
-  }
-
-  return arr.join('');
+  return cipher
+    .split('')
+    .filter((el, idx) => {
+      return idx % code === code-1;
+    })
+    .join('');
 }
