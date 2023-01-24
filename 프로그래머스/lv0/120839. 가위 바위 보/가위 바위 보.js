036f03cd19910ee25ddca2ab6ab6d004058a5d3f
조민수 <105564451+Choalstn@users.noreply.github.com>
2023-01-24 13:17:30 +0900

    [level 0] Title: 가위 바위 보, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,12 +1,11 @@
 function solution(rsp) {
     // 가위 2 바위 0 보 5
-    let arr = [];
-    
-    for(let i=0; i<rsp.length; i++) {
-        if(rsp[i] === "2") arr.push("0")
-        if(rsp[i] === "0") arr.push("5")
-        if(rsp[i] === "5") arr.push("2")  
-    }
-    
-    return arr.join('')
+  let obj = {
+    2: 0,
+    0: 5,
+    5: 2,
+  };
+
+  const answer = [...rsp].map((v) => obj[v]).join('');
+  return answer;
 }
