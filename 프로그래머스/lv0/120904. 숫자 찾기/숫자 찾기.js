bc4aae101958c8893f6ca57c1e34b2d9c8be0042
조민수 <105564451+Choalstn@users.noreply.github.com>
2023-01-27 13:05:26 +0900

    [level 0] Title: 숫자 찾기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.js
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.js
@@ -1,9 +1,3 @@
 function solution(num, k) {
-  let result = -1;
-  let arr = String(num).split('');
-
-  for (let i = 0; i < arr.length; i++) {
-    if(arr[i] === String(k)) return result = i+1;
-  }
-  return result;
+    return num.toString().split("").map((el) => Number(el)).indexOf(k) + 1 || -1
 }
