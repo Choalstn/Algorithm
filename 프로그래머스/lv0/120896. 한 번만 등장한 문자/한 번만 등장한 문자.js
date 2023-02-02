521174da55db50dc962ae3a80c573cd66c478d0e
조민수 <105564451+Choalstn@users.noreply.github.com>
2023-02-02 18:16:59 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -1,20 +1,5 @@
 function solution(s) {
-  let obj = {};
-  let result = [];
-
-  for (let i = 0; i < s.length; i++) {
-    if (!obj.hasOwnProperty(s[i])) {
-      obj[s[i]] = 1;
-    } else {
-      obj[s[i]] += 1;
-    }
-  }
-
-  for (const [key, value] of Object.entries(obj)) {
-    if (value === 1) {
-      result.push(key);
-    }
-  }
-
-  return result.sort().join('');
+    let res = [];
+    for (let c of s) if (s.indexOf(c) === s.lastIndexOf(c)) res.push(c);
+    return res.sort().join('');
 }
