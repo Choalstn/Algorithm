853448b44531606f432eb72b86e96fc4c4bfe39d
조민수 <105564451+Choalstn@users.noreply.github.com>
2023-02-05 22:27:07 +0900

    [level 0] Title: 공 던지기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
@@ -1,6 +1,3 @@
 function solution(numbers, k) {
-  const goNext = current => (current + 2) % numbers.length;
-  let current = 0;
-  for (let i = 0; i < k - 1; i++) current = goNext(current);
-  return numbers[current];
+    return numbers[(--k*2)%numbers.length];
 }
