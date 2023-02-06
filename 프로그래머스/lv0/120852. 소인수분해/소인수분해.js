3bda4f3f8873b9291dda002ced41096e2e7bd948
조민수 <105564451+Choalstn@users.noreply.github.com>
2023-02-06 14:54:59 +0900

    [level 0] Title: 소인수분해, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120852. 소인수분해/소인수분해.js
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.js
@@ -1,17 +1,15 @@
 function solution(n) {
-    let arr = [];
-    
-    for (let i = 2; i <= Math.sqrt(n); i++) {
+    var answer = [];
+
+    for(let i = 2; i <= n; i++) {
+
         while (n % i === 0) {
-            n /= i;
-            arr.push(i);
+
+            n = n / i;
+            answer.push(i);
+
         }
     }
-    
-    if (n >= 2) {
-        arr.push(n);
-    }
-    
-    let result = Array.from(new Set(arr));
-    return result.sort((a,b) => a-b)
+
+    return [...new Set(answer)];
 }
