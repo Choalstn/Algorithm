19b3bf8255b8c7af0c2af2f6a3efd0e9e6c3f8c1
조민수 <105564451+Choalstn@users.noreply.github.com>
2023-02-08 12:00:00 +0900

    [level 0] Title: 등수 매기기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
@@ -1,20 +1,5 @@
 function solution(score) {
-  let arr = Array.from({ length: score.length }, () => 1);
-  console.log(arr);
-
-  let a = score
-    .map((el1) => {
-      return el1.reduce((acc, curr) => acc + curr);
-    })
-    .map((el) => el / 2);
-
-  console.log(a);
-
-  for (let i = 0; i < a.length; i++) {
-    for (let j = 0; j < a.length; j++) {
-      if (a[i] < a[j]) arr[i]++;
-    }
-  }
-
-  return arr;
+  let newArr = score.map((el) => (el[0] + el[1]) / 2);
+  let sort = Array.from(newArr).sort((a, b) => b - a);
+  return newArr.map((v) => sort.indexOf(v) + 1);
 }
