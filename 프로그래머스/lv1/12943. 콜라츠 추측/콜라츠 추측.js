061092053469c75548413c44a691acf164ac7fb9
조민수 <105564451+Choalstn@users.noreply.github.com>
2023-03-09 01:51:23 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,14 +1,9 @@
 function solution(num) {
     let count = 0;
     while(num !== 1) {
-        if(num % 2 === 0) {
-            num /= 2;
-            count++;
-        } else {
-            num = (num*3)+1;
-            count++;
-        }
-        
+        (num % 2 === 0) ? num /= 2 : num = (num*3)+1;
+        count++
+         
         if(count >= 500) {
             return -1
         }
