eca0ffbe76b19f5c447b68eaef94e2b8043c506a
조민수 <105564451+Choalstn@users.noreply.github.com>
2023-03-11 16:11:29 +0900

    [level 1] Title: 내적, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.js
+++ 프로그래머스/lv1/70128. 내적/내적.js
@@ -1,8 +1,3 @@
 function solution(a, b) {
-    let result = 0;
-    for(let i=0; i<a.length; i++) {
-        result += (a[i]*b[i]);
-    }
-    
-    return result
+    return a.reduce((acc,curr,idx) => acc += curr*b[idx], 0)
 }
