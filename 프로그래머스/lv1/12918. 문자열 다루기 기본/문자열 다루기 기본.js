29eca91ebb7e11ae4db7194909e368bdefd11fca
조민수 <105564451+Choalstn@users.noreply.github.com>
2023-03-13 16:37:29 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,16 +1,14 @@
 function solution(s) {
-    let arr;
+  let arr;
   let result = false;
 
   arr = s.split('').map((el) => Number(el));
 
-  if (s.length === 4 || s.length === 6) {
-    if (arr.includes(NaN)) {
-      result = false;
-    } else {
-      result = true;
-    }
-  }
+  s.length === 4 || s.length === 6
+    ? arr.includes(NaN)
+      ? (result = false)
+      : (result = true)
+    : (result = false);
 
   return result;
 }
