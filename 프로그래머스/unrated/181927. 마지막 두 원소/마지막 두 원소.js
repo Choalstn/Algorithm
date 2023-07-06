7c5d4c72a360775528b6d25221f974bfaee4c0e0
조민수 <105564451+Choalstn@users.noreply.github.com>
2023-07-06 21:50:00 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,12 +1,4 @@
 function solution(num_list) {
-    const length = num_list.length
-    if(num_list[length-1] > num_list[length -2]) {
-        num_list.push(num_list[length-1]-num_list[length -2])
-
-    } else {
-        num_list.push(num_list[length -1] *2)
-
-    }
-    
-    return num_list
+  const [a, b] = [...num_list].reverse();
+  return [...num_list, a > b ? a - b : a * 2];
 }
