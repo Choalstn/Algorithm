cec829a433ba2f12a43f39e67dd9d21d6881985f
조민수 <105564451+Choalstn@users.noreply.github.com>
2023-07-07 01:07:12 +0900

    [unrated] Title: 조건 문자열, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,18 @@
+function solution(ineq, eq, n, m) {
+  let result;
+  if (ineq === '<') {
+    if (eq === '=') {
+      result = n <= m;
+    } else {
+      result = n < m;
+    }
+  } else {
+    if (eq === '=') {
+      result = n >= m;
+    } else {
+      result = n > m;
+    }
+  }
+
+  return result ? 1 : 0;
+}
