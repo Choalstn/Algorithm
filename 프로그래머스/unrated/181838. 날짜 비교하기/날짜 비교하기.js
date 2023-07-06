39776fae72a950aac5b52ffbf426764acd6ff15c
조민수 <105564451+Choalstn@users.noreply.github.com>
2023-07-07 00:56:49 +0900

    [unrated] Title: 날짜 비교하기, Time: 0.11 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,18 +1,3 @@
 function solution(date1, date2) {
-  let result = 0;
-  if (date1[0] === date2[0]) {
-    if (date1[1] === date2[1]) {
-      if (date1[2] === date2[2]) {
-        result = 0;
-      } else if (date1[2] < date2[2]) {
-        result = 1;
-      }
-    } else if (date1[1] < date2[1]) {
-      result = 1;
-    }
-  } else if (date1[0] < date2[0]) {
-    result = 1;
-  }
-
-  return result;
+    return new Date(date1) < new Date(date2) ? 1 : 0
 }
