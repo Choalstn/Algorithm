0701f8aa23f986a994094af63986c96ca4872ee4
조민수 <105564451+Choalstn@users.noreply.github.com>
2023-07-09 02:17:55 +0900

    [level 2] Title: 기능개발, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.js
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -1,25 +1,17 @@
 function solution(progresses, speeds) {
-  let deday = [];
   let result = [];
 
-  const minus = progresses.map((el) => 100 - el);
-  console.log(minus); //[ 7, 70, 45 ]
+  const days = progresses.map((el, idx) => Math.ceil((100 - el) / speeds[idx]));
 
-  for (let i = 0; i < speeds.length; i++) {
-    deday.push(Math.ceil(minus[i] / speeds[i]));
-  }
-
-  console.log(deday);
-
-  while (deday.length > 0) {
-    let last = deday.findIndex((el) => deday[0] < el);
+  while (days.length > 0) {
+    let last = days.findIndex((el) => days[0] < el);
 
     if (last === -1) {
-      result.push(deday.length);
-      deday.splice(0, deday.length);
+      result.push(days.length);
+      days.splice(0, days.length);
     } else {
       result.push(last);
-      deday.splice(0, last);
+      days.splice(0, last);
     }
   }
 
