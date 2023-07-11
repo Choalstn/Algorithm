7a8b1bfe73c9ae767493b645a13c4adb3f4fd19a
조민수 <105564451+Choalstn@users.noreply.github.com>
2023-07-11 16:42:28 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 2.97 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -0,0 +1,17 @@
+function solution(s) {
+  const words = s.split('');
+  console.log(words);
+  let nextUpper = true;
+  const converted = words.map((c) => {
+    if (c == ' ') {
+      nextUpper = true;
+      return c;
+    } else if (nextUpper) {
+      nextUpper = false;
+      return c.toUpperCase();
+    } else {
+      return c.toLowerCase();
+    }
+  });
+  return converted.join('');
+}
