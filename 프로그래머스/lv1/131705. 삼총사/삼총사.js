23c530e3b8b49923f1fe8f3b0dac95d20f1ed90f
조민수 <105564451+Choalstn@users.noreply.github.com>
2023-08-03 16:42:42 +0900

    [level 1] Title: 삼총사, Time: 0.38 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.js
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -1,17 +1,17 @@
 function solution(number) {
-  let all = [];
-  let result = [];
+  let result = 0;
 
-  for (let i = 0; i < number.length; i++) {
-    for (let j = i + 1; j < number.length; j++) {
-      for (let z = j + 1; z < number.length; z++) {
-        all.push([number[i], number[j], number[z]]);
-        if (all[all.length - 1].reduce((acc, crr) => acc + crr) === 0) {
-          result.push(all[all.length - 1]);
-        }
-      }
+  const combination = (current, start) => {
+
+    if (current.length === 3) {
+      result += current.reduce((acc, cur) => acc + cur, 0) === 0 ? 1 : 0;
+      return;
     }
-  }
 
-  return result.length;
+    for (let i = start; i < number.length; i++) {
+      combination([...current, number[i]], i + 1);
+    }
+  };
+  combination([], 0);
+  return result;
 }
