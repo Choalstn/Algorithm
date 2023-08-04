7292014d351bae39284b434275d38fc8a8a8dbec
조민수 <105564451+Choalstn@users.noreply.github.com>
2023-08-04 15:15:07 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 4.28 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,11 +1,11 @@
 function solution(t, p) {
-  let arr = [];
-  for (let i = 0; i < t.length; i++) {
-    arr.push(t.slice(i, i + p.length));
+  let count = 0;
+  for (let i = 0; i <= t.length - p.length; i++) {
+    console.log(i);
+    const value = t.slice(i, i + p.length);
+    console.log(value);
+    value <= +p && count++;
   }
 
-  return arr
-    .filter((el) => el.length >= p.length)
-    .map((el) => +el)
-    .filter((el) => el <= +p).length;
+  return count;
 }
