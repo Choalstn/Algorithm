ddd30fc08308f28e079f7a9896d22824636bc116
조민수 <105564451+Choalstn@users.noreply.github.com>
2023-08-08 18:26:39 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.21 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,9 +1,7 @@
 function solution(n, arr1, arr2) {
-  return arr1.map((el, idx) =>
-    (el | arr2[idx])
-      .toString(2)
-      .padStart(n, 0)
-      .replace(/0/g, ' ')
-      .replace(/1/g, '#')
-  );
+    return arr1.map((v, i) => addZero(n, (v | arr2[i]).toString(2)).replace(/1|0/g, a => +a ? '#' : ' '));
+}
+
+const addZero = (n, s) => {
+    return '0'.repeat(n - s.length) + s;
 }
