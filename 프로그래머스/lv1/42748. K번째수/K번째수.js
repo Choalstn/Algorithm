16f95f20c5a444f07868917e61ad38afa922ab9b
조민수 <105564451+Choalstn@users.noreply.github.com>
2023-08-14 16:09:59 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -0,0 +1,13 @@
+function solution(array, commands) {
+  let result = [];
+
+  for (let i = 0; i < commands.length; i++) {
+    const newArr = array
+      .slice(commands[i][0] - 1, commands[i][1])
+      .sort((a, b) => a - b);
+
+    result.push(newArr[commands[i][2] - 1]);
+  }
+
+  return result;
+}
