1e8392d08cc6cd4d4f864ba0e4c28ae6eaccd1ce
조민수 <105564451+Choalstn@users.noreply.github.com>
2023-08-15 16:22:46 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,21 +1,8 @@
 function solution(food) {
-  let arr = [];
-  let str = '';
-  let reverseStr = '0';
-
-  for (let i = 1; i < food.length; i++) {
-    if (food[i] % 2 !== 0) {
-      arr.push((food[i] - (food[i] % 2)) / 2);
-    } else {
-      arr.push(food[i] / 2);
+    let res = '';
+    for (let i = 1; i < food.length; i++) {
+        res += String(i).repeat(Math.floor(food[i]/2));
     }
-  }
 
-  for (let i = 0; i < arr.length; i++) {
-    str += String(i + 1).repeat(arr[i]);
-  }
-
-  reverseStr += str.split('').reverse().join('');
-
-  return str + reverseStr;
+    return res + '0' + [...res].reverse().join('');
 }
